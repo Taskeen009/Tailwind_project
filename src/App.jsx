80cebee7b8b42f7695739a8f286008892f31e5bc
Taskeen <taskeen.fa@gmail.com>
2024-11-02 02:19:41 +0530

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+const App=()=>{
+  return(
+    <main className="relative">hello
+    <section className="x1:padding-1 wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">Popular Products</section>
+    <section className="padding">SuperQuality</section>
+    <section className="padding-x py-10">Services</section>
+    <section className="padding">Special Offer</section>
+    <section className=" bg-pale-blue  padding">CustomerReview</section>
+    <section className="padding-x sm:py-32 w-full">Subscribe</section>
+    <section className="padding">Footer</section>
+    </main>
+  )
+}
+
+export default App;
